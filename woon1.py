b14777856d36d693d93cf0094434fe6b274c18da
Woon TheGreat <94958932+WoonTheGreat@users.noreply.github.com>
2021-12-27 23:15:13 +0800

    Update woon1.py

--- woon1.py
+++ woon1.py
@@ -1,17 +1,26 @@
 import streamlit as st
 import pandas as pd
 import sweetviz as sv
+import codecs
+import streamlit.components.v1 as components
 
 
 st.set_page_config(
     page_title="LayZii Project 1",
     page_icon="random",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto",
     menu_items={
         'About': "LayZii first project!"
     }
 )
+
+
+def st_display_sweetviz(report_html, height=900):
+    report_file = codecs.open(report_html, 'r')
+    page = report_file.read()
+    components.html(page, height=height, scrolling=True)
+
 
 st.title('Data Exploring App')
 
@@ -23,3 +32,4 @@ if uploaded_file is not None:
 # usw sweetviz to analyze table & show report
     my_report = sv.analyze(df)
     my_report.show_html()
+    st_display_sweetviz("SWEETVIZ_REPORT.html")
